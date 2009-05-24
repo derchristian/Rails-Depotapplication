400d4948301b6f83b957ee2a1b062ef6e5e6d8dc
christian2 User <christian2@ubuntu.ubuntu-domain>
2009-05-24 13:08:09 +0200

    End of Chapter Buidling an Application Tesk D: Add a Dash of Ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,13 +1,19 @@
 class StoreController < ApplicationController
   def index
 	  @products = Product.find_products_for_sale
+	  @cart = find_cart
 	  @time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+	  @count = increment_count
   end
   
   def add_to_cart
 	  product = Product.find(params[:id])
 	  @cart = find_cart
-	  @cart.add_product(product)
+	  @current_item = @cart.add_product(product)
+	  respond_to do |format|
+		  format.js if request.xhr?
+		  format.html {redirect_to_index}
+	  end
   rescue ActiveRecord::RecordNotFound
 	  logger.error("Attempt to acess invalid product #{params[:id]}")
 	  redirect_to_index("Invalid product")
@@ -15,18 +21,24 @@ class StoreController < ApplicationController
   
   def empty_cart
 	  session[:cart] = nil
-	  redirect_to_index("Your cart is currently empty")
+	  redirect_to_index unless request.xhr?
   end
 
   private
   
-  def redirect_to_index(msg)
-	  flash[:notice] = msg
+  def redirect_to_index(msg = nil)
+	  flash[:notice] = msg if msg
 	  redirect_to :action => 'index'
   end
   
   def find_cart
 	  session[:cart] ||= Cart.new
   end
+  
+  def increment_count
+	  session[:counter] ||= 0
+	  session[:counter] += 1
+  end
+  	  
 
 end
