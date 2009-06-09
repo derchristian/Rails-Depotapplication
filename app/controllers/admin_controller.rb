1c6bf9a07cb9ecfdc0dfdc3d8347024a8f77934f
christian2 User <christian2@ubuntu.ubuntu-domain>
2009-06-10 00:13:34 +0200

    End of Task G

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,26 @@
-#---
-# Excerpted from "Agile Web Development with Rails, 2nd Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails2 for more book information.
-#---
 class AdminController < ApplicationController
-  scaffold :product
-end
+  def login
+    if request.post?
+      user = User.authenticate(params[:name], params[:password])
+      if user
+        session[:user_id] = user.id
+        uri = session[:original_uri]
+        session[:original_uri] = nil
+        redirect_to(uri || { :action => "index" })
+      else
+        flash.now[:notice] = "Invalid user/password combination"
+      end
+    end      
+  end
 
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to(:action => "login")
+  end
+
+  def index
+    @total_orders = Order.count
+  end
+
+end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,15 @@
 class StoreController < ApplicationController
+
+  before_filter :find_cart, :except => :empty_cart
+
   def index
-	  @products = Product.find_products_for_sale
-	  @cart = find_cart
+	  @products = Product.find_products_for_sale	  
 	  @time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
 	  @count = increment_count
   end
   
   def add_to_cart
-	  product = Product.find(params[:id])
-	  @cart = find_cart
+	  product = Product.find(params[:id])	  
 	  @current_item = @cart.add_product(product)
 	  respond_to do |format|
 		  format.js if request.xhr?
@@ -24,6 +25,27 @@ class StoreController < ApplicationController
 	  redirect_to_index unless request.xhr?
   end
 
+  def checkout    
+    if @cart.items.empty?
+        redirect_to_index("Your cart is empty")
+    else
+        @order = Order.new
+    end
+  end
+
+  def save_order    
+    @order = Order.new(params[:order]) 
+    #@order.add_line_items_from_cart(@cart)
+    @order.build_from_cart(@cart)
+    if @order.save                     
+      session[:cart] = nil
+      redirect_to_index("Thank you for your order")
+    else
+      render :action => 'checkout'
+    end
+  end
+
+
   private
   
   def redirect_to_index(msg = nil)
@@ -32,13 +54,17 @@ class StoreController < ApplicationController
   end
   
   def find_cart
-	  session[:cart] ||= Cart.new
+	  @cart = (session[:cart] ||= Cart.new)
   end
   
   def increment_count
 	  session[:counter] ||= 0
 	  session[:counter] += 1
   end
-  	  
+
+  protected
+
+    def authorize
+    end  	  
 
 end
