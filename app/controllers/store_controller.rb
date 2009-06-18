467116d8307f1698581093c2c7412db168b6f9af
christian2 <christian2@ubuntu.ubuntu-domain>
2009-06-19 00:19:17 +0200

    End of Task I

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,70 +1,92 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 3rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails3 for more book information.
+#---
+
 class StoreController < ApplicationController
 
-  before_filter :find_cart, :except => :empty_cart
-
+      
+      before_filter :find_cart, :except => :empty_cart
+      
+  
   def index
-	  @products = Product.find_products_for_sale	  
-	  @time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-	  @count = increment_count
+    @products = Product.find_products_for_sale
   end
+  
+
+
+  
   
   def add_to_cart
-	  product = Product.find(params[:id])	  
-	  @current_item = @cart.add_product(product)
-	  respond_to do |format|
-		  format.js if request.xhr?
-		  format.html {redirect_to_index}
-	  end
+    product = Product.find(params[:id])
+    @current_item = @cart.add_product(product)
+      respond_to do |format|
+        format.js if request.xhr?
+        format.html {redirect_to_index}
+      end
   rescue ActiveRecord::RecordNotFound
-	  logger.error("Attempt to acess invalid product #{params[:id]}")
-	  redirect_to_index("Invalid product")
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid product")
   end
   
-  def empty_cart
-	  session[:cart] = nil
-	  redirect_to_index unless request.xhr?
-  end
 
-  def checkout    
+
+  
+  
+  def checkout
     if @cart.items.empty?
-        redirect_to_index("Your cart is empty")
+      redirect_to_index("Your cart is empty")
     else
-        @order = Order.new
+      @order = Order.new
     end
   end
+  
 
-  def save_order    
-    @order = Order.new(params[:order]) 
-    #@order.add_line_items_from_cart(@cart)
-    @order.build_from_cart(@cart)
-    if @order.save                     
+  
+  def save_order
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(@cart)
+    if @order.save
       session[:cart] = nil
-      redirect_to_index("Thank you for your order")
+      redirect_to_index(I18n.t('flash.thanks'))
     else
       render :action => 'checkout'
     end
   end
+  
 
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index
+  end
+  
 
-  private
+private
+
   
   def redirect_to_index(msg = nil)
-	  flash[:notice] = msg if msg
-	  redirect_to :action => 'index'
+    flash[:notice] = msg if msg
+    redirect_to :action => 'index'
   end
   
-  def find_cart
-	  @cart = (session[:cart] ||= Cart.new)
-  end
   
-  def increment_count
-	  session[:counter] ||= 0
-	  session[:counter] += 1
+
+      
+      def find_cart
+        @cart = (session[:cart] ||= Cart.new)
+      end
+      
+
+
+
+  #...
+protected
+
+  def authorize
   end
-
-  protected
-
-    def authorize
-    end  	  
-
 end
+
---
+++ config/initializers/i18n.rb
@@ -0,0 +1,9 @@
+# Initialize the i18n modul
+# for the depot projekt
+
+I18n.default_locale = 'en'
+LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
+LANGUAGES = {
+  'English' => 'en' ,
+  "Espa\xc3\xb1ol" => 'es'
+}
